3a5ef26dc6cedaa538111cd6925f3608c6b31dd1
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-10-05 02:18:14 +0000

    Release updated to v0.0.56

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.55"
+var version = "v0.0.56"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
