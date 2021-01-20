b6c267938d02145023fac7aa4ec8697cfac5705b
gajan <gajan@jp.ibm.com>
2021-01-20 03:21:48 +0000

    fixed

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.212"
+var version = "v0.0.213"
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
